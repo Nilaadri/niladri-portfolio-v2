897c5f04af5d06373dee5b53150c79afccc9315d
Niladri Dhar <135070110+Nilaadri@users.noreply.github.com>
2026-07-08 01:53:45 +0530

    feat(loader): add boot sequence

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import Desktop from './components/Desktop/Desktop';
+import { BootLoader } from "./components/Loader/Loader";
+import Desktop from "./components/Desktop/Desktop";
 
 export default function App() {
-  return Desktop();
+  return `
+    ${BootLoader()}
+    ${Desktop()}
+  `;
 }
